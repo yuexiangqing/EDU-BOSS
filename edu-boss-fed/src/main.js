614b93b4609d6a21f2a68c0426d8dda254cf1583
yuexiaogou <xiangqing0914@icloud.com>
2022-12-12 15:42:13 +0800

    样式处理

--- edu-boss-fed/src/main.js
+++ edu-boss-fed/src/main.js
@@ -6,7 +6,11 @@ import store from './store'
 // 引入 element
 import ElementUI from 'element-ui'
 // 引入 element 的主题文件
-import 'element-ui/lib/theme-chalk/index.css'
+// import 'element-ui/lib/theme-chalk/index.css'
+
+// 引入自定义的全局样式文件
+import './styles/index.scss'
+
 // 将 element 注册为 vue插件
 Vue.use(ElementUI)
 
