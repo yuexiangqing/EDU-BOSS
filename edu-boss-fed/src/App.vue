388c6a4026fec735431dbaa03011edc625a5f2d7
yuexiaogou <xiangqing0914@icloud.com>
2022-12-12 15:07:35 +0800

    调整初始目录

---
+++ edu-boss-fed/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>拉钩教育</h1>
+    <!-- 根路由出口 -->
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+
+</style>
---
+++ edu-boss-fed/src/router/index.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+// 路由规则
+const routes = [
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
