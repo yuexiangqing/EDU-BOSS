76d3df2efb5b68a545507856a6c3f61dcdef75dd
yuexiaogou <xiangqing0914@icloud.com>
2022-12-13 15:07:35 +0800

    接口处理-封装请求模块

--- edu-boss-fed/src/App.vue
+++ edu-boss-fed/src/App.vue
@@ -5,6 +5,21 @@
   </div>
 </template>
 
+<script>
+import request from '@/utils/request'
+
+request({
+  method: 'GET',
+  // url: '/front/ad/getAdList'
+  url: '/boss/v2/api-docs?group=edu-boss-boot'
+}).then(res => {
+  console.log(res)
+})
+
+export default {
+  name: 'App'
+}
+</script>
 <style lang="scss">
 
 </style>
---
+++ edu-boss-fed/src/utils/request.js
@@ -0,0 +1,25 @@
+import axios from 'axios'
+
+// create 创建 axios 实例
+// 用create是为了创建一个axios实例，create里面可以传入一个对象，这个对象用来对axios进行配置处理
+const request = axios.create({
+  timeout: 2000
+// baseURL:
+// headers:
+})
+
+function getBaseURL (url) {
+  if (url.startsWith('./boss')) {
+    return 'http://eduboss.lagounews.com'
+  } else {
+    return 'http://edufront.lagounews.com'
+  }
+}
+// 请求拦截器
+request.interceptors.request.use(function (config) {
+// 判断 config.url 的前缀，来进行请求 baseURL 的设置
+
+  config.baseURL = getBaseURL(config.url)
+  return config
+})
+export default request
